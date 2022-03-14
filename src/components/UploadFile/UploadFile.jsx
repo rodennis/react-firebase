f2fd952e4d5067ed16d30e6bb0c87dc7beb3425c
rodennis <rev.rltodd@gmail.com>
2022-03-13 20:02:41 -0500

    add css file to size picture, display picture on page

--- src/components/UploadFile/UploadFile.jsx
+++ src/components/UploadFile/UploadFile.jsx
@@ -1,43 +1,49 @@
-import React, {useState} from 'react'
-import {ref, uploadBytesResumable, getDownloadURL} from '@firebase/storage'
-import { storage } from '../../firebase/firebase'
-
+import React, { useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "@firebase/storage";
+import { storage } from "../../firebase/firebase";
+import "./UploadFile.css";
 
 function UploadFile() {
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0);
+  const [picUrl, setPicUrl] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const file = e.target[0].files[0]
-    upLoadFiles(file)
-  }
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    upLoadFiles(file);
+  };
 
-  const upLoadFiles = file => {
-    if(!file) return 
-    const storageRef = ref(storage, `/files/${file.name}`)
-    const uploadTask = uploadBytesResumable(storageRef, file)
+  const upLoadFiles = (file) => {
+    if (!file) return;
+    const storageRef = ref(storage, `/files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on("state_changed", (snapshot) => {
-      const prog = Math.round(
-        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      )
-      setProgress(prog)
-    }, (err) => console.log(err),
-        () => {
-            getDownloadURL(uploadTask.snapshot.ref)
-            .then(url => console.log(url))
-        }
-    )
-  }
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(prog);
+      },
+      (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => setPicUrl(url));
+      }
+    );
+  };
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-          <input type="file" className='input'/>
-          <button>Upload</button>
-        </form>
-        <h3>Uploaded {progress} %</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="file" className="input" />
+        <button>Upload</button>
+      </form>
+      <br />
+      <h3>Uploaded {progress} %</h3>
+      <br />
+      <img className="uploadedPicture" src={picUrl} alt="" />
     </div>
-  )
+  );
 }
-export default UploadFile
+export default UploadFile;
